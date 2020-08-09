9240d3ae5281c65136195e04616e343ad00111d5
Tom Hockett <tom.hockett@gmail.com>
2020-08-09 17:05:46 -0500

    init robocop and clean up boilerplate

--- Gemfile
+++ Gemfile
@@ -4,9 +4,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
+gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 gem 'shortener'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -25,7 +25,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'rubocop', require: false
 end
 
 group :development do
@@ -33,4 +34,4 @@ group :development do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_215822) do
-
+ActiveRecord::Schema.define(version: 20_200_809_215_822) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "shortened_urls", id: :serial, force: :cascade do |t|
-    t.integer "owner_id"
-    t.string "owner_type", limit: 20
-    t.text "url", null: false
-    t.string "unique_key", limit: 10, null: false
-    t.string "category"
-    t.integer "use_count", default: 0, null: false
-    t.datetime "expires_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["category"], name: "index_shortened_urls_on_category"
-    t.index ["owner_id", "owner_type"], name: "index_shortened_urls_on_owner_id_and_owner_type"
-    t.index ["unique_key"], name: "index_shortened_urls_on_unique_key", unique: true
-    t.index ["url"], name: "index_shortened_urls_on_url"
+  create_table 'shortened_urls', id: :serial, force: :cascade do |t|
+    t.integer 'owner_id'
+    t.string 'owner_type', limit: 20
+    t.text 'url', null: false
+    t.string 'unique_key', limit: 10, null: false
+    t.string 'category'
+    t.integer 'use_count', default: 0, null: false
+    t.datetime 'expires_at'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.index ['category'], name: 'index_shortened_urls_on_category'
+    t.index %w[owner_id owner_type], name: 'index_shortened_urls_on_owner_id_and_owner_type'
+    t.index ['unique_key'], name: 'index_shortened_urls_on_unique_key', unique: true
+    t.index ['url'], name: 'index_shortened_urls_on_url'
   end
-
 end
